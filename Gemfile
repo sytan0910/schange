eb1feb1231788c65eba94f1befe688536314795e
sheyong <sheyong5499@gmail.com>
2015-06-11 17:01:53 +0800

    Added devise

--- Gemfile
+++ Gemfile
@@ -47,5 +47,6 @@ gem 'twitter-bootstrap-rails', :git => 'git://github.com/seyhunak/twitter-bootst
 
 gem 'simple_form'
 
+gem 'devise'
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_for :models
   root 'pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
